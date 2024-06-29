922c0aa796f0f709112ab3726031cb31932f54c3
Errabi Mohamed Amine <errabi.ami@gmail.com>
2024-06-29 14:21:36 +0100

    KAN-2-Sandbox Create the product entity

---
+++ src/main/java/com/errabi/sandbox/entities/Product.java
@@ -0,0 +1,21 @@
+package com.errabi.sandbox.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String status;
+    private boolean visibility;
+    private String color;
+}
---
+++ src/main/java/com/errabi/sandbox/web/model/ProductDto.java
@@ -0,0 +1,28 @@
+package com.errabi.sandbox.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.*;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+    private Long id;
+    @NotBlank(message = "Product name is mandatory")
+    @Size(max = 30, message = "Product name must not exceed 30 characters")
+    private String name;
+    @NotBlank(message = "Please add a description")
+    private String description;
+    @NotBlank(message = "Please add product status")
+    private String status;
+    @NotBlank(message = "Please add product visibility")
+    private boolean visibility;
+    @NotBlank(message = "Color is mandatory")
+    @Pattern(regexp = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$", message = "Invalid color format. It should be a HEX color.")
+    private String color;
+}
