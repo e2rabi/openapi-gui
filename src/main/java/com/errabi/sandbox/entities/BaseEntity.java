cde3692196fc80505f7636581d032b58b87024f4
errabi ayoub <errabi.ayoub@gmail.com>
2024-06-29 16:00:26 +0100

    KAN-2- : Refactor code

---
+++ src/main/java/com/errabi/sandbox/entities/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.errabi.sandbox.entities;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+    @CreatedDate
+    protected LocalDateTime created ;
+    @LastModifiedDate
+    protected LocalDateTime lastModified ;
+    @CreatedBy
+    protected String createdBy ;
+    @LastModifiedBy
+    protected String lastModifiedBy ;
+    @Version
+    protected int version ;
+}
--- src/main/java/com/errabi/sandbox/web/mapper/ProductMapper.java
+++ src/main/java/com/errabi/sandbox/web/mapper/ProductMapper.java
@@ -4,8 +4,9 @@ import com.errabi.sandbox.entities.Product;
 import com.errabi.sandbox.web.model.ProductDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProductMapper {
     Product toEntity(ProductDto productDto);
     ProductDto toDto(Product product);
