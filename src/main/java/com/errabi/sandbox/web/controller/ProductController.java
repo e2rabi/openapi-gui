abaf5f2b06836310fb63ac6cec8099f3e8d7b825
Errabi Mohamed Amine <errabi.ami@gmail.com>
2024-06-30 20:17:15 +0100

    KAN-3-Create generic service to handle exception

--- src/main/java/com/errabi/sandbox/web/controller/ProductController.java
+++ src/main/java/com/errabi/sandbox/web/controller/ProductController.java
@@ -2,12 +2,11 @@ package com.errabi.sandbox.web.controller;
 
 import com.errabi.sandbox.services.ProductService;
 import com.errabi.sandbox.web.model.ProductDto;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
