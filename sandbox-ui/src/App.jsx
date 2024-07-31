ab0008a801e4fe57ab3eef712543fafe5338ff47
errabi ayoub <errabi.ayoub@gmail.com>
2024-07-31 10:26:09 +0100

    KAN-33 - Setup sandbox layout

--- sandbox-ui/src/App.jsx
+++ sandbox-ui/src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
-import './App.css'
 
+import Header from "./components/layout/Header/Header.jsx"
+import Footer from "./components/layout/Footer/Footer.jsx"
+import Navbar from "./components/layout/Navbar/Navbar.jsx"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/home/Home.jsx";
+import Login from "./components/login/Login.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 function App() {
 
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="container">
+      <Header />
+      <Navbar />
+      <main className="main" >
+        <RouterProvider router={router} />
+      </main>
+      <Footer />
+    </div>
+
   )
 }
 
