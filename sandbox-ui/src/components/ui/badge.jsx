cb4a0e6a5a984d8566f903a4d30eae4ba99c89e9
errabi ayoub <errabi.ayoub@gmail.com>
2024-08-02 19:06:29 +0100

    KAN-42 - Refactor user component part 2

--- sandbox-ui/src/components/ui/badge.jsx
+++ sandbox-ui/src/components/ui/badge.jsx
@@ -14,6 +14,10 @@ const badgeVariants = cva(
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        disabled:
+          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        enabled:
+          "border-transparent bg-green-700  text-destructive-foreground hover:bg-green-700/80",
         outline: "text-foreground",
       },
     },
