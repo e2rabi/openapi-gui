5469a04a45292111fcffaf08510ab404d0df178d
errabi ayoub <errabi.ayoub@gmail.com>
2024-08-09 12:26:29 +0100

    KAN-52 - Refactor user filter component

--- sandbox-ui/src/components/user/UserEdit.jsx
+++ sandbox-ui/src/components/user/UserEdit.jsx
@@ -10,35 +10,38 @@ import {
 import ReactDOM from 'react-dom';
 import React from "react";
 
+const Portal = ({ children, containerId }) => {
+    const container = document.getElementById(containerId);
+    return ReactDOM.createPortal(children, container);
+};
 
-const UserEdit = ({ isOpen, setIsOpen }) => {
-    const dialogElement = (
-        <Dialog open={isOpen}
-            onOpenChange={() => setIsOpen(false)}
-            closeOnEsc={true}
-            aria-label="Sample Dialog"
-            closeOnOverlayClick={true}
-            isDismissable={true}
-        >
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Are you absolutely sure?</DialogTitle>
-                    <DialogDescription>
-                        This action cannot be undone. This will permanently delete your account
-                        and remove your data from our servers.
-                    </DialogDescription>
-                </DialogHeader>
-                <DialogFooter className="sm:justify-start">
-                    <DialogClose asChild />
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    );
+const UserEditDialog = ({ isOpen, setIsOpen }) => {
     return (
-        <>
-            {/* Your existing code */}
-            {ReactDOM.createPortal(dialogElement, document.getElementById('dialog-root'))}
-        </>
-    )
+        <Portal containerId="dialog-portal">
+            <Dialog open={isOpen}
+                onOpenChange={() => setIsOpen(false)}
+                closeOnEsc={true}
+                aria-label="edit user"
+                closeOnOverlayClick={true}
+                isDismissable={true}
+            >
+
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Edit user</DialogTitle>
+                        <DialogDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter className="sm:justify-start">
+                        <DialogClose asChild />
+                    </DialogFooter>
+                </DialogContent>
+
+            </Dialog>
+        </Portal>
+    );
 }
-export default React.memo(UserEdit)
+const UserEdit = React.memo(UserEditDialog);
+export default UserEdit;
