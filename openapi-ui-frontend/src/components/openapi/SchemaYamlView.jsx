7a73dde3269f9c483ae6a00fea29a8e4323143f4
errabi ayoub <errabi.ayoub@gmail.com>
2024-08-31 22:01:12 +0100

    KAN-67 - OpenApi - Add schema yaml viewer

---
+++ openapi-ui-frontend/src/components/openapi/SchemaYamlView.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { useSelector } from 'react-redux';
+import yaml from 'js-yaml';
+
+const customStyle = {
+    ...oneDark,
+    'pre[class*="language-"]': {
+        ...oneDark['pre[class*="language-"]'],
+        background: 'transparent',
+    },
+    'code[class*="language-"]': {
+        ...oneDark['code[class*="language-"]'],
+        background: 'transparent',
+    },
+};
+const yamlCode = `
+components:
+  schemas:
+    Order:
+      type: object
+      properties:
+        id:
+          type: integer
+          format: int64
+          example: 10
+        petId:
+          type: integer
+          format: int64
+          example: 198772
+    `;
+const MemoizedSchemaYamlView = () => {
+    const schemas = useSelector((state) => state.schema.value)
+
+    const generateYaml = () => {
+        const yamlObject = {
+            components: {
+                schemas: schemas.reduce((acc, schema) => {
+                    acc[schema.name] = {
+                        type: 'object',
+                        properties: schema.properties,
+                    };
+                    return acc;
+                }, {}),
+            },
+        };
+        return yaml.dump(yamlObject);
+    }
+    return (
+        <SyntaxHighlighter language="yaml" style={customStyle} className="w-full bg-transparent">
+            {generateYaml()}
+        </SyntaxHighlighter>
+    )
+}
+const SchemaYamlView = React.memo(MemoizedSchemaYamlView)
+export default SchemaYamlView
