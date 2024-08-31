560c6ce232e9a4d5e3ca8ae69a77f564105239db
errabi ayoub <errabi.ayoub@gmail.com>
2024-08-31 11:22:02 +0100

    KAN-67 - OpenApi - add schema operation

--- openapi-ui-frontend/src/components/openapi/Schema.jsx
+++ openapi-ui-frontend/src/components/openapi/Schema.jsx
@@ -1,6 +1,4 @@
-import React, { useRef, useState } from 'react'
-import { useForm } from "react-hook-form"
-
+import React, { useDeferredValue, useEffect, useState } from 'react'
 import {
     Card,
     CardContent,
@@ -10,53 +8,53 @@ import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 import { PlusCircle, CopyCheck, FilePenLine, Trash2 } from "lucide-react"
 import { useDispatch } from 'react-redux'
-import { addSchema } from './SchemaSlice'
-const Schema = ({ name }) => {
-    const dispatch = useDispatch()
-    const hiddenSubmitButtonRef = useRef(null);
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm()
+import { addSchema, removeSchema, updateSchema } from './SchemaSlice'
 
-    const onSubmit = (data) => {
-        if (data) {
-            dispatch(addSchema(data.schemaName))
-        }
-    }
+const MemoizedSchema = ({ name }) => {
+    const [schemaName, setSchemaName] = useState("newSchema")
+    const schemaNameDeffered = useDeferredValue(schemaName);
+    const dispatch = useDispatch()
+
     const createSchema = () => {
-        hiddenSubmitButtonRef.current.click();
+        dispatch(addSchema("newSchema"))
+
     }
+    const deleteSchema = (schemaName) => {
+        dispatch(removeSchema(schemaName))
+    }
+    const updateSchemaName = (name, value) => {
+        setSchemaName(() => value)
+        dispatch(updateSchema({ name, value }))
+    }
+
+    useEffect(() => {
+        setSchemaName(name)
+    }, [name])
     return (
         <Card className="w-full m-2">
             <CardContent>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="w-full">
-                        <div className="flex items-center flex-row space-x-2 justify-start relative mt-6">
-                            <Label htmlFor="name">Schema</Label>
-                            <Button variant="outline" size="icon" onClick={() => createSchema()}>
-                                <PlusCircle className="h-4 w-4" />
-                            </Button>
-                            <Button variant="outline" size="icon">
-                                <CopyCheck className="h-4 w-4" />
-                            </Button>
-                            <Button variant="outline" size="icon">
-                                <FilePenLine className="h-4 w-4" />
-                            </Button>
-                            <Button variant="outline" size="icon">
-                                <Trash2 className="h-4 w-4" />
-                            </Button>
-                            <Input value={name} id="schemaName" placeholder="New Schema" {...register("schemaName", { required: true })} />
-                            {errors.schemaName && <span className="text-red-500 text-sm  mt-2 relative">SchemaName is required</span>}
-                            <input ref={hiddenSubmitButtonRef} hidden type="submit" />
-
-                        </div>
+                <div className="w-full">
+                    <div className="flex items-center flex-row space-x-2 justify-start relative mt-6">
+                        <Label htmlFor="name">Schema</Label>
+                        <Button variant="outline" size="icon" onClick={() => createSchema()}>
+                            <PlusCircle className="h-4 w-4" />
+                        </Button>
+                        <Button variant="outline" size="icon">
+                            <CopyCheck className="h-4 w-4" />
+                        </Button>
+                        <Button variant="outline" size="icon">
+                            <FilePenLine className="h-4 w-4" />
+                        </Button>
+                        <Button variant="outline" size="icon" onClick={() => deleteSchema(name)}>
+                            <Trash2 className="h-4 w-4" />
+                        </Button>
+                        <Input name="schemaName" value={schemaNameDeffered} id="schemaName" placeholder="New Schema" onChange={(e) => updateSchemaName(name, e.target.value)} />
+                        {/* {errors.schemaName && <span className="text-red-500 text-sm  mt-2 relative">SchemaName is required</span>} */}
                     </div>
-                </form>
+                </div>
             </CardContent>
         </Card>
     )
 }
-
+const Schema = React.memo(MemoizedSchema)
 export default Schema
